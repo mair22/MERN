34de0977d52897ecef7c5ee521b9c0730da2def1
mair22 <acediawalker@gmail.com>
2022-08-25 11:52:55 +0700

    register-login

--- routes/auth.js
+++ routes/auth.js
@@ -44,9 +44,55 @@ router.post("/register", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error!!!" });
   }
 });
 
-router.get("/", (req, res) => res.send("USER ROUTE"));
+//@route POST api/auth/login
+//@desc Login user
+//access Public
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  //Simple Vadilation
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username and/or password" });
+  try {
+    //Check for existing user
+    const user = await User.findOne({ username });
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
+
+    //Username found
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid)
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
+
+    //All good
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    res.json({
+      success: true,
+      message: "Logged in successfully",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error!!!" });
+  }
+});
 
 module.exports = router;
