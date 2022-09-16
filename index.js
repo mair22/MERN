09eb218ae924a2c6b91da0ddc648418542bf630c
mair22 <acediawalker@gmail.com>
2022-09-16 12:24:32 +0700

    test deploy

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
